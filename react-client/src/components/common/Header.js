df892f07c5fc76a50dad9c9541b618b2cee9787c
petahcole <cbatson89@gmail.com>
2017-06-28 09:26:56 -0600

    remove webpack

---
+++ react-client/src/components/common/Header.js
@@ -0,0 +1,11 @@
+import React from react;
+
+const Header = () =>  {
+  return  (
+    <nav className="navbar navbar-fixed-top">
+      <div className="container">
+        <h1>Have a Drink</h1>
+      </div>
+    </nav>
+  )
+}
